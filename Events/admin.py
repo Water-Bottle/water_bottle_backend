73df6a74e1082ffc601086b4c5da103f81a16a28
ameetbora <ameetbora@yahoo.com>
2016-06-18 21:25:08 +0530

    doing update

--- Events/admin.py
+++ Events/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from models import Event, Announcements, UserProfile
 
-# Register your models here.
+class MyUserProfile(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2','first_name','last_name')}
+        ),
+    )
+
+admin.site.register(Event)
+admin.site.register(Announcements)
+admin.site.unregister(User)
+admin.site.register(User, MyUserProfile)
+admin.site.register(UserProfile)
--- Events/models.py
+++ Events/models.py
@@ -1,17 +1,39 @@
 from __future__ import unicode_literals
-
+import uuid 
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from random import randint
+from django.contrib.auth.models import User
 
 class Event(models.Model):
 	event_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	name = models.CharField(max_length=255)
 	event_date = models.DateField()
 	event_start_time = models.TimeField()
-	event_image = models.CharField(max_length=255)
+	event_image = models.CharField(max_length=255, blank=True)
 	event_description = models.TextField()
 	event_url = models.CharField(max_length=255, unique=True)
-	pin_code = models.IntegerField(validators=[MinValueValidator(1000),MaxValueValidator(9999)])
+	pin_code = models.IntegerField(validators=[MinValueValidator(1000),MaxValueValidator(9999)], unique=True)
+	created_by = models.OneToOneField(User)
 
-	def save():
-		
+	def save(self):
+		#super(Event, self).save()
+		date = datetime.datetime.today
+		self.event_url = '%i/%i/%i/%i' % (date.year, date.month, date.day, self.event_id)
+		pin_code = randint(1000,9999)
+
+		super(Event, self).save()
+
+
+class Announcements(models.Model):
+	title = models.CharField(max_length=255)
+	description = models.TextField()
+	event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	profile_picture = models.CharField(max_length=255, blank=True)
+	is_hackchat_admin = models.CharField(max_length=1, choices=(("Y","Yes"),("N","No"),), default="N")
+
+
